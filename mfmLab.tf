2ea2c87f25a368927344e634e7ffd412407b648f
Stephan Marvin <157425765+sjmarvin@users.noreply.github.com>
2024-12-18 08:08:06 -0800

    Update mfmLab.tf

--- mfmLab.tf
+++ mfmLab.tf
@@ -1,105 +1,69 @@
 terraform {
   required_providers {
     equinix = {
-      source = "equinix/equinix"
-    }
+      source  = "equinix/equinix"
+     }
   }
 }
-provider "equinix" {
-  auth_token    = var.auth_token
-  client_id     = var.equinix_client_id
+ # Configuration options 
+  # Credentials for only Equinix Metal resources provider "equinix" {
+  auth_token = var.auth_token
+  client_id = var.equinix_client_id
   client_secret = var.equinix_client_secret
+
 }
-
-## allocate same vlans for both Metros
-
-resource "equinix_metal_vlan" "VLAN" {
-  description = "Metal VLAN"
-  metro       = var.aside
+# Create a new VLAN in metro "da"
+resource "equinix_metal_vlan" "vlan1" {
+  description = "VLAN in Dallas"
+  metro       = var.metro1
   project_id  = var.metal_project_id
-  vxlan       = var.vxlan
+  vxlan       = 47
 }
 
-
-## create metal Aside
-resource "equinix_metal_device" "server_A" {
-  hostname         = "AsideServer"
+# Create a new server in metro "da"
+resource "equinix_metal_device" "test" {
+  hostname         = "sjmLab1"
   plan             = var.plan
-  metro            = var.aside
+  metro            = var.metro1
   operating_system = var.operating_system
   billing_cycle    = "hourly"
   project_id       = var.metal_project_id
-  user_data        = data.cloudinit_config.config1.rendered
+  user_data = format("#!/bin/bash\napt update\napt install vlan\nmodprobe 8021q\necho '8021q' >> /etc/modules-load.d/networking.conf\nip link add link bond0 name bond0.%g type vlan id %g\nip addr add 192.168.100.10/24 brd 192.168.100.255 dev bond0.%g\nip link set dev bond0.%g up", equinix_metal_vlan.vlan1.vxlan, equinix_metal_vlan.vlan1.vxlan, equinix_metal_vlan.vlan1.vxlan, equinix_metal_vlan.vlan1.vxlan)
 }
 
-data "cloudinit_config" "config1" {
-  gzip          = false # not supported on Equinix Metal
-  base64_encode = false # not supported on Equinix Metal
-
-  part {
-    content_type = "text/cloud-config"
-    content = templatefile("${path.module}/cloud-config1.cfg", {
-      VLAN_ID_0  = equinix_metal_vlan.my_vlan1.vxlan
-    })
-  }
+resource "equinix_metal_port_vlan_attachment" "test" {
+  device_id = equinix_metal_device.test.id
+  port_name = "bond0"
+  vlan_vnid = equinix_metal_vlan.vlan1.vxlan
 }
 
-resource "equinix_metal_device_network_type" "port_type_test1" {
-  device_id = equinix_metal_device.server_A.id
-  type      = "hybrid-bonded"
-}
-
-resource "equinix_metal_port_vlan_attachment" "vlan_attach_test1" {
-  device_id = equinix_metal_device_network_type.port_type_test1.id
-  port_name = "bond0"  
-  vlan_vnid = equinix_metal_vlan.my_vlan1.vxlan
-}
-
-## create metal node2
-
-resource "equinix_metal_vlan" "my_vlan2" {
-  description = "Metal's metro VLAN"
-  metro       = var.zside
+# Create a new VLAN in metro "sv"
+resource "equinix_metal_vlan" "vlan2" {
+  description = "VLAN in Silicon Valley"
+  metro       = var.metro2
   project_id  = var.metal_project_id
-  vxlan       = var.vxlan
+  vxlan       = 47
 }
-resource "equinix_metal_device" "metal_node2" {
-  hostname         = "ZsideServer"
+
+# Create a new Server in metro "sv"
+resource "equinix_metal_device" "test2" {
+  hostname         = "sjmLab2"
   plan             = var.plan
-  metro            = var.zside
+  metro            = var.metro1
   operating_system = var.operating_system
   billing_cycle    = "hourly"
   project_id       = var.metal_project_id
-  user_data        = data.cloudinit_config.config2.rendered
+  user_data = format("#!/bin/bash\napt update\napt install vlan\nmodprobe 8021q\necho '8021q' >> /etc/modules-load.d/networking.conf\nip link add link bond0 name bond0.%g type vlan id %g\nip addr add 192.168.100.20/24 brd 192.168.100.255 dev bond0.%g\nip link set dev bond0.%g up", equinix_metal_vlan.vlan2.vxlan, equinix_metal_vlan.vlan2.vxlan, equinix_metal_vlan.vlan2.vxlan,equinix_metal_vlan.vlan2.vxlan)
 }
 
-data "cloudinit_config" "config2" {
-  gzip          = false # not supported on Equinix Metal
-  base64_encode = false # not supported on Equinix Metal
-
-  part {
-    content_type = "text/cloud-config"
-    content = templatefile("${path.module}/cloud-config2.cfg", {
-      VLAN_ID_0  = equinix_metal_vlan.my_vlan2.vxlan
-    })
-  }
+resource "equinix_metal_port_vlan_attachment" "test2" {
+  device_id = equinix_metal_device.test2.id
+  port_name = "bond0"
+  vlan_vnid = equinix_metal_vlan.vlan2.vxlan
 }
 
-resource "equinix_metal_device_network_type" "port_type_test2" {
-  device_id = equinix_metal_device.server_Z.id
-  type      = "hybrid-bonded"
-}
-
-resource "equinix_metal_port_vlan_attachment" "vlan_attach_test2" {
-  device_id = equinix_metal_device_network_type.port_type_test2.id
-  port_name = "bond0" 
-  vlan_vnid = equinix_metal_vlan.my_vlan2.vxlan
-}
-
-## Create a VC via dedciated port in metro1
-
-/* this is the "Interconnection ID" of the "DA-Metal-to-Fabric-Dedicated-Redundant-Port" via Metal's portal*/
-
+## Create VC via dedicated port in metro "da"
+## this is the "Interconnection ID" of the "DA-Metal-to-Fabric-Dedicated-Redundant-Port" via Metal's portal
 data "equinix_metal_connection" "metro1_port" {
   connection_id = var.conn_id
 }
@@ -108,36 +72,33 @@ resource "equinix_metal_virtual_circuit" "metro1_vc" {
   connection_id = var.conn_id
   project_id    = var.metal_project_id
   port_id       = data.equinix_metal_connection.metro1_port.ports[0].id
-  vlan_id       = equinix_metal_vlan.my_vlan1.vxlan
-  nni_vlan      = equinix_metal_vlan.my_vlan1.vxlan
-  name          = "sjm-vc-tf"
+  vlan_id       = equinix_metal_vlan.vlan1.vxlan
+  nni_vlan      = equinix_metal_vlan.vlan1.vxlan
+  name          = "sjm-tf-vc"
 }
-
-
-## Request a Metal shared connection and get a z-side token from Metal
-
+## Request a Metal connection and get a z-side token from Metal
 resource "equinix_metal_connection" "example" {
   name               = "sjm-tf-metal-port"
   project_id         = var.metal_project_id
   type               = "shared"
   redundancy         = "primary"
-  metro              = var.zside
+  metro              = var.metro2
   speed              = "10Gbps"
   service_token_type = "z_side"
   contact_email      = "smarvin@equinix.com"
-  vlans              = [equinix_metal_vlan.my_vlan2.vxlan]
+  vlans              = [equinix_metal_vlan.vlan2.vxlan]
 }
 
-## Use the token from "equinix_metal_connectio.example" to setup VC in fabric portal. 
-## A-side port is  your Metal owned dedicated port in Equinix Fabric portal
+## Use the token from "equinix_metal_connection.example" to setup VC in fabric portal:
+## A-side port is your Metal owned dedicated port in Equinix Fabric portal
 
-resource "equinix_fabric_connection" "fabConn" {
-  name = "sjm-tf-metalport-fabric"
+resource "equinix_fabric_connection" "this" {
+  name = "sjm-metalport-fabric"
   type = "EVPL_VC"
   bandwidth = 50
   notifications {
     type   = "ALL"
-    emails = ["sjm@equinix.com"]
+    emails = ["smarvin@equinix.com"]
   }
   order {
     purchase_order_number = ""
@@ -146,14 +107,13 @@ resource "equinix_fabric_connection" "fabConn" {
     access_point {
       type = "COLO"
       port {
-        uuid = var.aside_port
+        uuid = var.aside_port.id
       }
       link_protocol {
         type     = "DOT1Q"
-        vlan_tag = equinix_metal_vlan.my_vlan1.vxlan
+        vlan_tag = equinix_metal_vlan.vlan1.vxlan
       }
-      location {
-        metro_code  = var.aside
+        metro_code = var.metro1
       }
     }
   }
@@ -162,40 +122,4 @@ resource "equinix_fabric_connection" "fabConn" {
       uuid = equinix_metal_connection.example.service_tokens.0.id
     }
   }
-}
-
-## Output the metal node names and IPs, so you can login via 'ssh root@IP' 
-
-output "Aside_name" {
-  value       = equinix_metal_device.metal_node1.hostname
-  description = "Your metal_node1 hostname:"
-}
-
-output "Aside_IP" {
-  value       = equinix_metal_device.metal_node1.access_public_ipv4
-  description = "Your metal_node1 IP:"
-}
-
-output "My_Metro1_VLAN" {
-  value = equinix_metal_vlan.my_vlan1.vxlan
-}
-
-output "Zside_name" {
-  value       = equinix_metal_device.metal_node2.hostname
-  description = "Your metal_node2 hostname:"
-}
-
-output "Zside_IP" {
-  value       = equinix_metal_device.metal_node2.access_public_ipv4
-  description = "Your metal_node2 IP:"
-}
-output "Zside_VLAN" {
-  value = equinix_metal_vlan.my_vlan2.vxlan
-}
-
-output "Z_side_Service_Token" {
-  value = equinix_metal_connection.example.service_tokens.0.id
-}
-output "Z_side_VC_UUID" {
-  value = equinix_fabric_connection.fabConn.id
 }
